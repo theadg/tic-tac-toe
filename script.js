098716b20bd4dc51af953f8a29e915a7057b66d2
theadg <deguzmanbernardandrew@gmail.com>
2022-10-19 21:02:04 +0800

    Finalize game winning logic

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const gameBoard = (function () {
     ["", "", ""],
   ];
   let currentSymbol = "X";
+  let prevSymbol = "";
   const gameBoardRowOne = Array.from(document.querySelectorAll(".row--1"));
   const gameBoardRowTwo = Array.from(document.querySelectorAll(".row--2"));
   const gameBoardRowThree = Array.from(document.querySelectorAll(".row--3"));
@@ -15,8 +16,10 @@ const gameBoard = (function () {
   const switchSymbol = () => {
     if (currentSymbol === "X") {
       currentSymbol = "O";
+      prevSymbol = "X";
     } else {
       currentSymbol = "X";
+      prevSymbol = "O";
     }
   };
 
@@ -36,23 +39,27 @@ const gameBoard = (function () {
 
         if (index >= 0 && index <= 2) {
           board[0].splice(index, 1, currentSymbol);
-        } else if (index > 2 && index <= 4) {
+          console.log(`${index} added row one`);
+        } else if (index >= 3 && index <= 5) {
           board[1].splice(index - 3, 1, currentSymbol);
-        } else {
+          console.log(`${index} added row two`);
+        } else if (index >= 6 && index <= 9) {
           board[2].splice(index - 6, 1, currentSymbol);
+          console.log(`${index} added row three`);
         }
 
         switchSymbol();
         // console.log(board[0]);
         // console.log(board[1]);
         // console.log(board[2]);
+        checkWinner();
       };
     });
   };
 
-  //   TODO: Make this dynamic
+  //   TODO: Make this dynamic DONE
   const horizontalWinner = (element) => {
-    return element === "X";
+    return element === prevSymbol;
   };
 
   const endGame = () => {
@@ -64,34 +71,54 @@ const gameBoard = (function () {
   const checkWinner = () => {
     board.forEach((row, index) => {
       //horizontal winner
+      console.log(row);
       if (row.every(horizontalWinner)) {
-        console.log("victory!");
+        console.log("victory! horizontal");
         endGame();
       }
       //   verticalwinner;
     });
+
     // TODO: Create logic just to check answers if there are x numbers of elements in row
     // TODO: Apply this to every row
-    horizontalX = 0;
+
+    // this code should only run if all the a row is completed
     for (let i = 0; i < board.length; i++) {
-      if (board[i][0] == "X") {
-        horizontalX++;
-        if (horizontalX === 3) {
-          console.log("victory!");
+      let colOneElements = 0,
+        colTwoElements = 0,
+        colThreeElements = 0;
+      if (board[i][0] == prevSymbol) {
+        colOneElements++;
+        console.log(`COL ONE: ${colOneElements} ${prevSymbol}`);
+        if (colOneElements === 3) {
+          console.log("victory col one");
         }
-      } else if (board[i][1] == "X") {
-        horizontalX++;
-        console.log(horizontalX);
-        if (horizontalX === 3) {
-          console.log("victory!");
+      } else if (board[i][1] === prevSymbol) {
+        colTwoElements++;
+        console.log(`COL TWO: ${colTwoElements} ${prevSymbol}`);
+        if (colTwoElements === 3) {
+          console.log("victory! col two");
         }
-      } else if ((board[i][2] = "X")) {
-        horizontalX++;
-        // if (horizontalX === 3) console.log("victory!");
+      } else if (board[i][2] === prevSymbol) {
+        colThreeElements++;
+        console.log(`COL THREE: ${colThreeElements} ${prevSymbol}`);
+        if (colThreeElements === 3) {
+          console.log("victory! col three");
+        }
       }
     }
 
     // TODO: Create Diagonal Solution
+    if (
+      (board[0][0] === prevSymbol &&
+        board[1][1] === prevSymbol &&
+        board[2][2] === prevSymbol) ||
+      (board[0][2] === prevSymbol &&
+        board[1][1] === prevSymbol &&
+        board[2][0] === prevSymbol)
+    ) {
+      console.log("victory diagonal");
+    }
   };
   return { addClick, checkWinner };
 })();
